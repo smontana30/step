77c7cf39ee64cbf35e2d1e72bd0c8e752c0b90e0
Sofia Montana <smontana@knights.ucf.edu>
2020-06-15 15:30:49 +0000

    final

--- portfolio/src/main/java/com/google/sps/servlets/BlobFormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BlobFormHandlerServlet.java
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
@@ -45,78 +45,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import static java.util.Arrays.*;
 import java.util.List;
-
-
-/**
- * When the user submits the form, Blobstore processes the file upload and then forwards the request
- * to this servlet. This servlet can then process the request using the file URL we get from
- * Blobstore.
- */
-final class BlobImg {
-    private final String message;
-    private final String image;
-    private final long timestamp;
-    private final long id;
-    
-    public BlobImg(String message, String image, long timestamp, long id) {
-        this.message = message;
-        this.image = image;
-        this.timestamp = timestamp;
-        this.id = id;
-    }
-}
-
+ 
 @WebServlet("/form-blob")
 public class BlobFormHandlerServlet extends HttpServlet {
-
+ 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+ 
     // Get the message entered by the user.
     String message = request.getParameter("message");
-
+ 
     // Get the URL of the image that the user uploaded to Blobstore.
     String imageUrl = getUploadedFileUrl(request, "image");
-
+ 
     // Output some HTML that shows the data the user entered.
     // A real codebase would probably store these in Datastore.
-    // PrintWriter out = response.getWriter();
-    // out.println("<p>Here's the image you uploaded:</p>");
-    // out.println("<a href=\"" + imageUrl + "\">");
-    // out.println("<img src=\"" + imageUrl + "\" />");
-    // out.println("</a>");
-    // out.println("<p>Here's the text you entered:</p>");
-    // out.println(message);
+    PrintWriter out = response.getWriter();
+    out.println("<p>Here's the image you uploaded:</p>");
+    out.println("<a href=\"" + imageUrl + "\">");
+    out.println("<img src=\"" + imageUrl + "\" />");
+    out.println("</a>");
+    out.println("<p>Here's the text you entered:</p>");
+    out.println(message);
   }
-
+ 
   /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
   private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
     List<BlobKey> blobKeys = blobs.get("image");
-
+ 
     // User submitted form without selecting a file, so we can't get a URL. (dev server)
     if (blobKeys == null || blobKeys.isEmpty()) {
       return null;
     }
-
+ 
     // Our form only contains a single file input, so get the first index.
     BlobKey blobKey = blobKeys.get(0);
-
+ 
     // User submitted form without selecting a file, so we can't get a URL. (live server)
     BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
     if (blobInfo.getSize() == 0) {
       blobstoreService.delete(blobKey);
       return null;
     }
-
+ 
     // We could check the validity of the file here, e.g. to make sure it's an image file
     // https://stackoverflow.com/q/10779564/873165
-
+ 
     // Use ImagesService to get a URL that points to the uploaded file.
     ImagesService imagesService = ImagesServiceFactory.getImagesService();
     ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-
+ 
     // To support running in Google Cloud Shell with AppEngine's devserver, we must use the relative
     // path to the image, rather than the path returned by imagesService which contains a host.
     try {
--- portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadServlet.java
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.io.IOException;
@@ -21,7 +21,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 /**
  * When the fetch() function requests the /blobstore-upload-url URL, the content of the response is
  * the URL that allows a user to upload a file to Blobstore. If this sounds confusing, try running a
@@ -29,13 +29,13 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/blobstore-upload")
 public class BlobstoreUploadServlet extends HttpServlet {
-
+ 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     String uploadUrl = blobstoreService.createUploadUrl("/form-blob");
-
-    response.setContentType("application/json;");
+ 
+    response.setContentType("text/html");
     response.getWriter().println(uploadUrl);
   }
 }
